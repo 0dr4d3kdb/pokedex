aa74e891f07cac27432ba6cb38b2e0a3c793b219
0dr4d3kdb <danielbeta97@gmail.com>
2025-05-06 21:10:19 -0400

    Primera versión de gestión de usuarios Edutech

---
+++ Edutech-users/src/main/java/com/example/Edutech_users/EdutechUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.Edutech_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechUsersApplication.class, args);
+	}
+
+}
---
+++ Edutech-users/src/test/java/com/example/Edutech_users/EdutechUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Edutech_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
